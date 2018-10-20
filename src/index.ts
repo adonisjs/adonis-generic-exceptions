57a086a4fab2199c233468171f1c3b8f4c2ffc5b
thetutlage <virk.officials@gmail.com>
2018-10-20 17:25:16 +0200

    feat(rewrite): rewrite in typescript
    BREAKING CHANGE: Remove custom methods from InvalidArgumentException in favour of using ow

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+/*
+ * adonis-generic-exceptions
+ *
+ * (c) Harminder Virk <virk@adonisjs.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+*/
+
+export {
+  DomainException,
+  HttpException,
+  InvalidArgumentException,
+  RangeException,
+  LogicalException,
+} from'node-exceptions'
+
+export { RuntimeException } from './RuntimeException'
